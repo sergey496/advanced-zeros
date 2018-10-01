9ddba44f0d5c43f856bdf5224dc22a45330d2b17
Siarhei Zylevich <sergey496@yandex.ru>
2018-10-01 21:05:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-/* module.exports = function getZerosCount(number, base) {
-	let n=2;
-	let d=base;
-	let i=0;
-	let arrayTmp=[];
-	while ((n<=base)&(n<=d)){
-		if (base%n==0){
-			arrayTmp[i]=n;
-			i++;
-			base=Math.round(base/n);
-		}else{
-			n++;	
-		}
-	}
-	let sum=0;
-	for (let i=1;i<27;i++){	
-		sum+=Math.floor(number/Math.pow(arrayTmp[arrayTmp.length-1],i));
-	}
-	sum=Math.floor(sum/(arrayTmp.lastIndexOf(arrayTmp[arrayTmp.length-1])-arrayTmp.indexOf(arrayTmp[arrayTmp.length-1])+1));
-	return sum;
-}
- */
- 
  module.exports = function getZerosCount(number, base) {
 	let n=2;
 	let i=0;
